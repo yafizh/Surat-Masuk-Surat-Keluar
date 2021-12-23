5b8efd0da56e282dbaf88d519f689fdf16de13fb
yafizh <a.chairil.yafizh@gmail.com>
2021-12-23 20:45:05 +0800

    makes halaman_tampil_surat_keluar.php works

--- surat_keluar/halaman_tampil_surat_keluar.php
+++ surat_keluar/halaman_tampil_surat_keluar.php
@@ -32,61 +32,35 @@
                 </tr>
               </thead>
               <tbody>
-                <tr>
-                  <th class="align-middle" scope="row">1</th>
-                  <td class="align-middle">Designer</td>
-                  <td class="align-middle">5 Desember 2021</td>
-                  <td class="align-middle">Brandon Jacob</td>
-                  <td class="d-flex justify-content-center gap-1">
-                    <a type="button" class="btn btn-secondary"><i class="bi bi-eye"></i></a>
-                    <a type="button" class="btn btn-warning"><i class="bi bi-pencil"></i></a>
-                    <a type="button" class="btn btn-danger"><i class="bi bi-trash"></i></a>
-                  </td>
-                </tr>
-                <tr>
-                  <th class="align-middle" scope="row">2</th>
-                  <td class="align-middle">Developer</td>
-                  <td class="align-middle">9 Desember 2021</td>
-                  <td class="align-middle">Bridie Kessler</td>
-                  <td class="d-flex justify-content-center gap-1">
-                    <a type="button" class="btn btn-secondary"><i class="bi bi-eye"></i></a>
-                    <a type="button" class="btn btn-warning"><i class="bi bi-pencil"></i></a>
-                    <a type="button" class="btn btn-danger"><i class="bi bi-trash"></i></a>
-                  </td>
-                </tr>
-                <tr>
-                  <th class="align-middle" scope="row">3</th>
-                  <td class="align-middle">Finance</td>
-                  <td class="align-middle">30 Desember 2021</td>
-                  <td class="align-middle">Ashleigh Langosh</td>
-                  <td class="d-flex justify-content-center gap-1">
-                    <a type="button" class="btn btn-secondary"><i class="bi bi-eye"></i></a>
-                    <a type="button" class="btn btn-warning"><i class="bi bi-pencil"></i></a>
-                    <a type="button" class="btn btn-danger"><i class="bi bi-trash"></i></a>
-                  </td>
-                </tr>
-                <tr>
-                  <th class="align-middle" scope="row">4</th>
-                  <td class="align-middle">HR</td>
-                  <td class="align-middle">15 Februari 2022</td>
-                  <td class="align-middle">Angus Grady</td>
-                  <td class="d-flex justify-content-center gap-1">
-                    <a type="button" class="btn btn-secondary"><i class="bi bi-eye"></i></a>
-                    <a type="button" class="btn btn-warning"><i class="bi bi-pencil"></i></a>
-                    <a type="button" class="btn btn-danger"><i class="bi bi-trash"></i></a>
-                  </td>
-                </tr>
-                <tr>
-                  <th class="align-middle" scope="row">5</th>
-                  <td class="align-middle">Dynamic Division Officer</td>
-                  <td class="align-middle">1 Januari 2022</td>
-                  <td class="align-middle">Raheem Lehner</td>
-                  <td class="d-flex justify-content-center gap-1">
-                    <a type="button" class="btn btn-secondary"><i class="bi bi-eye"></i></a>
-                    <a type="button" class="btn btn-warning"><i class="bi bi-pencil"></i></a>
-                    <a type="button" class="btn btn-danger"><i class="bi bi-trash"></i></a>
-                  </td>
-                </tr>
+                <?php
+                require_once "koneksi.php";
+                require_once "utils.php";
+
+                $no = 1;
+                $sql = "SELECT * FROM tabel_surat_keluar";
+                $result = $mysqli->query($sql);
+                ?>
+                <?php while ($row = $result->fetch_assoc()) : ?>
+                  <tr>
+                    <th class="align-middle" scope="row"><?= $no++; ?></th>
+                    <td class="align-middle"><?= $row['unit_pengolah']; ?></td>
+                    <td class="align-middle"><?= $row['tanggal_surat']; ?></td>
+                    <td class="align-middle"><?= $row['nomor_surat']; ?></td>
+                    <td class="d-flex justify-content-center gap-1">
+                      <button onclick="showDetail({
+                          'id_surat_keluar': '<?= $row['id_surat_keluar']; ?>',
+                          'unit_pengolah' : '<?= $row['unit_pengolah']; ?>',
+                          'tanggal_surat' : '<?= $row['tanggal_surat']; ?>',
+                          'nomor_surat' : '<?= $row['nomor_surat']; ?>',
+                          'perihal' : '<?= $row['perihal']; ?>',
+                          'tujuan_surat' : '<?= $row['tujuan_surat']; ?>',
+                          'dokumen_surat' : '<?= $row['dokumen_surat']; ?>'
+                        })" class="btn btn-secondary" data-bs-toggle="modal" data-bs-target="#basicModal"><i class="bi bi-eye"></i></button>
+                      <a href="index.php?page=edit_surat&item=edit_surat_keluar&id_surat_keluar=<?= $row['id_surat_keluar']; ?>" class="btn btn-warning"><i class="bi bi-pencil"></i></a>
+                      <a href="index.php?page=delete_surat&item=delete_surat_keluar&id_surat_keluar=<?= $row['id_surat_keluar']; ?>" class="btn btn-danger" onclick="return confirm('Yakin ingin menghapus data ini?')"><i class="bi bi-trash"></i></a>
+                    </td>
+                  </tr>
+                <?php endwhile; ?>
               </tbody>
             </table>
             <!-- End Table with stripped rows -->
@@ -99,3 +73,52 @@
   </section>
 
 </main><!-- End #main -->
+<!-- Basic Modal -->
+<div class="modal fade" id="basicModal" tabindex="-1">
+  <div class="modal-dialog">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h5 class="modal-title">Nomor Surat <span id="judul_nomor_surat"></span></h5>
+        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+      </div>
+      <div class="modal-body">
+        <div class="row mb-3">
+          <label class="col-sm-4 col-form-label">Unit Pengolah</label>
+          <label class="col-sm-7 col-form-label" id="unit_pengolah"></label>
+        </div>
+        <div class="row mb-3">
+          <label class="col-sm-4 col-form-label">Tanggal Surat</label>
+          <label class="col-sm-8 col-form-label" id="tanggal_surat"></label>
+        </div>
+        <div class="row mb-3">
+          <label class="col-sm-4 col-form-label">Nomor Surat</label>
+          <label class="col-sm-8 col-form-label" id="nomor_surat"></label>
+        </div>
+        <div class="row mb-3">
+          <label class="col-sm-4 col-form-label">Perihal</label>
+          <label class="col-sm-8 col-form-label" id="perihal"></label>
+        </div>
+        <div class="row mb-3">
+          <label class="col-sm-4 col-form-label">Tujuan Surat</label>
+          <label class="col-sm-8 col-form-label" id="tujuan_surat"></label>
+        </div>
+        <div class="row mb-3">
+          <label class="col-sm-4 col-form-label">Dokumen Surat</label>
+          <label class="col-sm-8 col-form-label"><a href="#" target="_blank" id="dokumen_surat">Klik Disini</a></label>
+        </div>
+      </div>
+    </div>
+  </div>
+</div><!-- End Basic Modal-->
+
+<script>
+  const showDetail = (surat_keluar) => {
+    document.querySelector(".modal-title #judul_nomor_surat").textContent = surat_keluar.nomor_surat;
+    document.querySelector(".modal-body #unit_pengolah").textContent = surat_keluar.unit_pengolah;
+    document.querySelector(".modal-body #nomor_surat").textContent = surat_keluar.nomor_surat;
+    document.querySelector(".modal-body #tanggal_surat").textContent = surat_keluar.tanggal_surat;
+    document.querySelector(".modal-body #perihal").textContent = surat_keluar.perihal;
+    document.querySelector(".modal-body #tujuan_surat").textContent = surat_keluar.tujuan_surat;
+    document.querySelector(".modal-body #dokumen_surat").setAttribute('href', 'surat_keluar/uploads/' + surat_keluar.dokumen_surat);
+  }
+</script>
