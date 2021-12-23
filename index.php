c90a7379e52aa3506ab3c4b705df818a43e47a11
yafizh <a.chairil.yafizh@gmail.com>
2021-12-23 21:25:15 +0800

    makes halaman_delete_surat_keluar.php works

--- index.php
+++ index.php
@@ -1,23 +1,25 @@
 <?php include_once "header.php"; ?>
-<?php 
-  if(isset($_GET['page'])){
-    if($_GET['page'] == 'tambah_surat' && $_GET['item'] == 'tambah_surat_masuk') {
-      include_once "surat_masuk/halaman_tambah_surat_masuk.php";      
-    } else if($_GET['page'] == 'tambah_surat' && $_GET['item'] == 'tambah_surat_keluar') {
-      include_once "surat_keluar/halaman_tambah_surat_keluar.php";      
-    } else if($_GET['page'] == 'tampil_surat' && $_GET['item'] == 'tampil_surat_masuk') {
-      include_once "surat_masuk/halaman_tampil_surat_masuk.php";      
-    } else if($_GET['page'] == 'tampil_surat' && $_GET['item'] == 'tampil_surat_keluar') {
-      include_once "surat_keluar/halaman_tampil_surat_keluar.php";      
-    } else if($_GET['page'] == 'edit_surat' && $_GET['item'] == 'edit_surat_masuk') {
-      include_once "surat_masuk/halaman_edit_surat_masuk.php";      
-    } else if($_GET['page'] == 'delete_surat' && $_GET['item'] == 'delete_surat_masuk') {
-      include_once "surat_masuk/halaman_delete_surat_masuk.php";      
-    } else if($_GET['page'] == 'edit_surat' && $_GET['item'] == 'edit_surat_keluar')
-      include_once "surat_keluar/halaman_edit_surat_keluar.php";  
-  } else {
-    include_once "halaman_beranda.php";
-  }
+<?php
+if (isset($_GET['page'])) {
+  if ($_GET['page'] == 'tambah_surat' && $_GET['item'] == 'tambah_surat_masuk')
+    include_once "surat_masuk/halaman_tambah_surat_masuk.php";
+  else if ($_GET['page'] == 'tambah_surat' && $_GET['item'] == 'tambah_surat_keluar')
+    include_once "surat_keluar/halaman_tambah_surat_keluar.php";
+  else if ($_GET['page'] == 'tampil_surat' && $_GET['item'] == 'tampil_surat_masuk')
+    include_once "surat_masuk/halaman_tampil_surat_masuk.php";
+  else if ($_GET['page'] == 'tampil_surat' && $_GET['item'] == 'tampil_surat_keluar')
+    include_once "surat_keluar/halaman_tampil_surat_keluar.php";
+  else if ($_GET['page'] == 'edit_surat' && $_GET['item'] == 'edit_surat_masuk')
+    include_once "surat_masuk/halaman_edit_surat_masuk.php";
+  else if ($_GET['page'] == 'delete_surat' && $_GET['item'] == 'delete_surat_masuk')
+    include_once "surat_masuk/halaman_delete_surat_masuk.php";
+  else if ($_GET['page'] == 'edit_surat' && $_GET['item'] == 'edit_surat_keluar')
+    include_once "surat_keluar/halaman_edit_surat_keluar.php";
+  else if ($_GET['page'] == 'delete_surat' && $_GET['item'] == 'delete_surat_keluar')
+    include_once "surat_keluar/halaman_delete_surat_keluar.php";
+} else {
+  include_once "halaman_beranda.php";
+}
 
 ?>
 <?php include_once "footer.php"; ?>
---
+++ surat_keluar/halaman_delete_surat_keluar.php
@@ -0,0 +1,16 @@
+<?php
+
+if (isset($_GET['id_surat_keluar'])) {
+    require_once "koneksi.php";
+
+    $sql = "DELETE FROM tabel_surat_keluar WHERE id_surat_keluar=" . $_GET['id_surat_keluar'];
+    if ($mysqli->query($sql) === TRUE) {
+        echo "<script>alert('Surat Keluar berhasil dihapus.')</script>";
+        echo "<script>" .
+            "window.location.href='index.php?page=tampil_surat&item=tampil_surat_keluar';" .
+            "</script>";
+    } else echo "Error: " . $sql . "<br>" . $mysqli->error;    
+} else
+    echo "<script>" .
+        "window.location.href='index.php?page=tampil_surat&item=tampil_surat_keluar';" .
+        "</script>";
