15e3860c4274bf36c8ad1406f988e66f361d7d07
yafizh <a.chairil.yafizh@gmail.com>
2021-12-31 00:13:31 +0800

    fixing kop surat pada report

--- laporan/laporan_surat_masuk_hari_ini.php
+++ laporan/laporan_surat_masuk_hari_ini.php
@@ -21,23 +21,28 @@
         .text-center {
             text-align: center;
         }
+
+        #kop {
+            display: flex;
+            justify-content: center;
+        }
     </style>
 </head>
 
 <body>
     <div class="container">
         <div id="kop" class="d-flex justify-content-center gap-5">
-            <img src="../kpu.png" height="100" alt="">
-            <div class="text-center">
+            <img src="../assets/img/Banjarbaru.png" height="150" alt="">
+            <div class="text-center" style="flex: 1;">
                 <h2>
-                    NAMA INSTANSI
+                    DINAS ARSIP DAN PERPUSTAKAAN DAERAH
                     <br>
-                    KABUPATEN BANJAR
+                    BANJARBARU
                 </h2>
                 <p>
-                    Alamat: Jalan Mentri 4
+                    Alamat: Jl. Wijaya Kusuma No.7
                     <br>
-                    Email: instansi@gmail.com
+                    Email: darpusdabjb@banjarbarukota.go.id
                 </p>
             </div>
         </div>
