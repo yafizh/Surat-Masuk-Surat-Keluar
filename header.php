7a769605348d3cd15ca0beb510676cf60854747e
yafizh <a.chairil.yafizh@gmail.com>
2022-01-20 09:54:49 +0800

    refactoring code sidebar

--- header.php
+++ header.php
@@ -99,36 +99,36 @@
         </ul>
       </li><!-- End Components Nav -->
       <li class="nav-item">
-        <a id="tampil_surat" class="nav-link collapsed" data-bs-target="#components-nav" data-bs-toggle="collapse" href="#">
-          <i class="ri ri-mail-line"></i><span>Data Surat</span><i class="bi bi-chevron-down ms-auto"></i>
+        <a id="surat_keluar" class="nav-link collapsed" data-bs-target="#components-nav" data-bs-toggle="collapse" href="#">
+          <i class="ri ri-mail-line"></i><span>Surat Keluar</span><i class="bi bi-chevron-down ms-auto"></i>
         </a>
         <ul id="components-nav" class="nav-content collapse " data-bs-parent="#sidebar-nav">
           <li>
-            <a id="tampil_surat_masuk" href="index.php?page=tampil_surat&item=tampil_surat_masuk">
-              <i class="bi bi-circle"></i><span>Surat Masuk</span>
+            <a id="tampil_surat_keluar" href="index.php?page=surat_keluar&item=tampil_surat_keluar">
+              <i class="bi bi-circle"></i><span>Data Surat Keluar</span>
             </a>
           </li>
           <li>
-            <a id="tampil_surat_keluar" href="index.php?page=tampil_surat&item=tampil_surat_keluar">
-              <i class="bi bi-circle"></i><span>Surat Keluar</span>
+            <a id="tambah_surat_keluar" href="index.php?page=surat_keluar&item=tambah_surat_keluar">
+              <i class="bi bi-circle"></i><span>Tambah Surat Keluar</span>
             </a>
           </li>
         </ul>
       </li><!-- End Components Nav -->
 
       <li class="nav-item" id="x">
-        <a id="tambah_surat" class="nav-link collapsed" data-bs-target="#forms-nav" data-bs-toggle="collapse" href="#">
-          <i class="ri-mail-add-line"></i><span>Tambah Surat</span><i class="bi bi-chevron-down ms-auto"></i>
+        <a id="surat_masuk" class="nav-link collapsed" data-bs-target="#forms-nav" data-bs-toggle="collapse" href="#">
+          <i class="ri-mail-add-line"></i><span>Surat Masuk</span><i class="bi bi-chevron-down ms-auto"></i>
         </a>
         <ul id="forms-nav" class="nav-content collapse " data-bs-parent="#sidebar-nav">
           <li>
-            <a id="tambah_surat_masuk" href="index.php?page=tambah_surat&item=tambah_surat_masuk">
-              <i class="bi bi-circle"></i><span>Surat Masuk</span>
+            <a id="tampil_surat_masuk" href="index.php?page=surat_masuk&item=tampil_surat_masuk">
+              <i class="bi bi-circle"></i><span>Data Surat Masuk</span>
             </a>
           </li>
           <li>
-            <a id="tambah_surat_keluar" href="index.php?page=tambah_surat&item=tambah_surat_keluar">
-              <i class="bi bi-circle"></i><span>Surat Keluar</span>
+            <a id="tambah_surat_masuk" href="index.php?page=surat_masuk&item=tambah_surat_masuk">
+              <i class="bi bi-circle"></i><span>Tambah Surat Masuk</span>
             </a>
           </li>
         </ul>
--- index.php
+++ index.php
@@ -1,29 +1,29 @@
 <?php include_once "header.php"; ?>
 <?php
 if (isset($_GET['page'])) {
-  if ($_GET['page'] == 'tambah_surat' && $_GET['item'] == 'tambah_surat_masuk')
+  if ($_GET['page'] == 'surat_masuk' && $_GET['item'] == 'tambah_surat_masuk')
     include_once "surat_masuk/halaman_tambah_surat_masuk.php";
-  else if ($_GET['page'] == 'tambah_surat' && $_GET['item'] == 'tambah_surat_keluar')
+  else if ($_GET['page'] == 'surat_keluar' && $_GET['item'] == 'tambah_surat_keluar')
     include_once "surat_keluar/halaman_tambah_surat_keluar.php";
   else if ($_GET['page'] == 'ruangan' && $_GET['item'] == 'tambah_ruangan')
     include_once "ruangan/halaman_tambah_ruangan.php";
   else if ($_GET['page'] == 'kode_surat' && $_GET['item'] == 'tambah_kode_surat')
     include_once "kode_surat/halaman_tambah_kode_surat.php";
-  else if ($_GET['page'] == 'tampil_surat' && $_GET['item'] == 'tampil_surat_masuk')
+  else if ($_GET['page'] == 'surat_masuk' && $_GET['item'] == 'tampil_surat_masuk')
     include_once "surat_masuk/halaman_tampil_surat_masuk.php";
-  else if ($_GET['page'] == 'tampil_surat' && $_GET['item'] == 'tampil_surat_keluar')
+  else if ($_GET['page'] == 'surat_keluar' && $_GET['item'] == 'tampil_surat_keluar')
     include_once "surat_keluar/halaman_tampil_surat_keluar.php";
   else if ($_GET['page'] == 'ruangan' && $_GET['item'] == 'tampil_ruangan')
     include_once "ruangan/halaman_tampil_ruangan.php";
   else if ($_GET['page'] == 'kode_surat' && $_GET['item'] == 'tampil_kode_surat')
     include_once "kode_surat/halaman_tampil_kode_surat.php";
-  else if ($_GET['page'] == 'edit_surat' && $_GET['item'] == 'edit_surat_masuk')
+  else if ($_GET['page'] == 'surat_masuk' && $_GET['item'] == 'edit_surat_masuk')
     include_once "surat_masuk/halaman_edit_surat_masuk.php";
-  else if ($_GET['page'] == 'delete_surat' && $_GET['item'] == 'delete_surat_masuk')
+  else if ($_GET['page'] == 'surat_masuk' && $_GET['item'] == 'delete_surat_masuk')
     include_once "surat_masuk/halaman_delete_surat_masuk.php";
-  else if ($_GET['page'] == 'edit_surat' && $_GET['item'] == 'edit_surat_keluar')
+  else if ($_GET['page'] == 'surat_keluar' && $_GET['item'] == 'edit_surat_keluar')
     include_once "surat_keluar/halaman_edit_surat_keluar.php";
-  else if ($_GET['page'] == 'delete_surat' && $_GET['item'] == 'delete_surat_keluar')
+  else if ($_GET['page'] == 'surat_keluar' && $_GET['item'] == 'delete_surat_keluar')
     include_once "surat_keluar/halaman_delete_surat_keluar.php";
   else if ($_GET['page'] == 'ruangan' && $_GET['item'] == 'edit_ruangan')
     include_once "ruangan/halaman_edit_ruangan.php";
