10ac994a9631950f70a0afc55406a11741f9b6a8
yafizh <a.chairil.yafizh@gmail.com>
2022-01-21 06:57:34 +0800

    change brand in login and navbar

--- auth/halaman_login.php
+++ auth/halaman_login.php
@@ -27,8 +27,8 @@ if (isset($_POST['submit'])) {
 
                         <div class="d-flex justify-content-center py-4">
                             <a href="index.html" class="logo d-flex align-items-center w-auto">
-                                <img src="assets/img/logo.png" alt="">
-                                <span class="d-none d-lg-block">NiceAdmin</span>
+                                <!-- <img src="assets/img/logo.png" alt=""> -->
+                                <!-- <span class="d-none d-lg-block">NiceAdmin</span> -->
                             </a>
                         </div><!-- End Logo -->
 
@@ -37,11 +37,18 @@ if (isset($_POST['submit'])) {
                             <div class="card-body">
 
                                 <div class="pt-4 pb-2">
-                                    <h5 class="card-title text-center pb-0 fs-4">Login to Your Account</h5>
-                                    <p class="text-center small">Enter your username & password to login</p>
+                                    <div class="d-flex">
+                                        <img src="assets/img/Banjarbaru.png" width="100" alt="">
+                                        <div class="d-flex flex-column">
+                                            <h5 class="card-title text-center pb-0 fs-5">Aplikasi Surat Menyurat</h5>
+                                            <p class="text-center">Dinas Arsip dan Perpustakaan Daerah Kota Banjarbaru</p>
+                                        </div>
+                                    </div>
+                                    <!-- <h5 class="card-title text-center pb-0 fs-4">Login</h5> -->
+                                    <!-- <p class="text-center small">Masukkan username & password</p> -->
                                 </div>
 
-                                <form class="row g-3 needs-validation" novalidate method="POST">
+                                <form class="row g-3 needs-validation mt-1" novalidate method="POST">
 
                                     <div class="col-12">
                                         <label for="username_user" class="form-label">Username</label>
@@ -60,14 +67,6 @@ if (isset($_POST['submit'])) {
                                 </form>
 
                             </div>
-                        </div>
-
-                        <div class="credits">
-                            <!-- All the links in the footer should remain intact. -->
-                            <!-- You can delete the links only if you purchased the pro version. -->
-                            <!-- Licensing information: https://bootstrapmade.com/license/ -->
-                            <!-- Purchase the pro version with working PHP/AJAX contact form: https://bootstrapmade.com/nice-admin-bootstrap-admin-html-template/ -->
-                            Designed by <a href="https://bootstrapmade.com/">BootstrapMade</a>
                         </div>
 
                     </div>
